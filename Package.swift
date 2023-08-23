a6cf106bc1b7a2de74f1c4661df6ccd84df0b2fa
Tom Kunst <thomas.m.kunst@gmail.com>
2023-08-23 17:50:05 +0200

    Dependencies bumped up to latest pre-release revision of TCA

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/tgrapperon/swift-dependencies-additions.git", from: "0.4.0"),
-        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", branch: "prerelease/1.0"),
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "0.57.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
